ab69ef44250f0aac0daaacea463f25ec98ed025f
Tunahan-Uysal <TunahanUysal26@gmail.com>
2023-09-22 14:12:31 +0300

    Added Components per suggestion

---
+++ src/components/UBlogs.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="basis-1/2 h-screen w-6/12 ml-16 flex flex-row justify-center border border-black">
+    <div class=" w-10/12 h-3/4 mt-10 bg-gray-500 rounded-lg">
+      <div class=" w-11/12 h-64 bg-slate-400 mt-4 mx-auto rounded-lg bg-opacity-gradient-to-b bg-contain">
+        <img src="https://images.unsplash.com/photo-1681506540686-126384e54751?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80" alt="" class="bg-contain">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+
+
+<style>
+
+.bg-opacity-gradient-to-b{
+    background: linear-gradient(to bottom, transparent, white),
+  }
+.bg-opacity-gradient-to-t{
+    background: linear-gradient(to top, transparent, white),
+  }
+</style>
---
+++ src/components/UHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="h-16 w-full flex flex-row bg-indigo-400" style="">
+    <a class="font-display font-black mx-auto text-xl text-white ml-4 basis-1/12">Tunahan Uysal</a>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-10 my-auto mr-4 h-10">
+      <path fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+    </svg>
+  </nav>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+
+
+<style>
+
+</style>
