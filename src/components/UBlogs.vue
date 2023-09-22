6678cd7154c143343c84756b3d80183027ade9f7
Tunahan-Uysal <TunahanUysal26@gmail.com>
2023-09-22 16:08:26 +0300

    Breka

--- src/components/UBlogs.vue
+++ src/components/UBlogs.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="basis-1/2 h-screen w-6/12 ml-16 flex flex-row justify-center border border-black">
-    <div class=" w-10/12 h-3/4 mt-10 bg-gray-500 rounded-lg flex flex-col">
+    <div class=" w-10/12 h-[512px] mt-10 bg-gray-500 rounded-lg flex flex-col">
       <div class="w-11/12 bg-slate-400 mt-4 mx-auto rounded-lg bg-cover bg-bottom basis-2/4" style="background-image: url(https://images.unsplash.com/photo-1681506540686-126384e54751?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80);">
       </div>
       <p class="mx-6 text-transparent bg-clip-text bg-gradient-to-b from-black via-black via-50 basis-1/4">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae aperiam nihil et minima est voluptatibus sit, odio totam assumenda saepe? Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, enim! Lorem ipsum dolor sit amet.
       </p>
-      <div class="basis-1/4 flex flex-row">
-        
+      <div class="basis-1/4 flex flex-row-reverse">
+        <UButton/>
       </div>
     </div>
   </div>
@@ -15,8 +15,9 @@
 
 <script lang="ts" setup>
 
-</script>
+import UButton from '~/components/UButton.vue'
 
+</script>
 
 
 <style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,42 @@ export default {
   ],
   theme: {
     extend: {
+      'animation': {
+        'gradient-x':'gradient-x 15s ease infinite',
+        'gradient-y':'gradient-y 15s ease infinite',
+        'gradient-xy':'gradient-xy 15s ease infinite',
+    },
+    'keyframes': {
+        'gradient-y': {
+            '0%, 100%': {
+                'background-size':'400% 400%',
+                'background-position': 'center top'
+            },
+            '50%': {
+                'background-size':'200% 200%',
+                'background-position': 'center center'
+            }
+        },
+        'gradient-x': {
+            '0%, 100%': {
+                'background-size':'200% 200%',
+                'background-position': 'left center'
+            },
+            '50%': {
+                'background-size':'200% 200%',
+                'background-position': 'right center'
+            }
+        },
+        'gradient-xy': {
+            '0%, 100%': {
+                'background-size':'400% 400%',
+                'background-position': 'left center'
+            },
+            '50%': {
+                'background-size':'200% 200%',
+                'background-position': 'right center'
+            }
+        },
       fontFamily: {
         'display': ['"Lexend Zetta"','"Lexend Mega"','"Roboto Mono"', 'ui-monospace']
       },
