6be6ee01d6268a4230909e6d844131cbb5d5db42
Tunahan-Uysal <TunahanUysal26@gmail.com>
2023-09-22 14:48:40 +0300

    Working on UBlogs.vue

--- src/components/UBlogs.vue
+++ src/components/UBlogs.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="basis-1/2 h-screen w-6/12 ml-16 flex flex-row justify-center border border-black">
-    <div class=" w-10/12 h-3/4 mt-10 bg-gray-500 rounded-lg">
-      <div class=" w-11/12 h-64 bg-slate-400 mt-4 mx-auto rounded-lg bg-opacity-gradient-to-b bg-contain">
-        <img src="https://images.unsplash.com/photo-1681506540686-126384e54751?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80" alt="" class="bg-contain">
+    <div class=" w-10/12 h-3/4 mt-10 bg-gray-500 rounded-lg flex flex-col">
+      <div class="w-11/12 bg-slate-400 mt-4 mx-auto rounded-lg bg-cover bg-bottom basis-2/4" style="background-image: url(https://images.unsplash.com/photo-1681506540686-126384e54751?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80);">
+      </div>
+      <p class="mx-6 text-transparent bg-clip-text bg-gradient-to-b from-black via-black via-50 basis-1/4">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae aperiam nihil et minima est voluptatibus sit, odio totam assumenda saepe? Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, enim! Lorem ipsum dolor sit amet.
+      </p>
+      <div class="basis-1/4 flex flex-row">
+        
       </div>
     </div>
   </div>
@@ -17,9 +22,12 @@
 <style>
 
 .bg-opacity-gradient-to-b{
-    background: linear-gradient(to bottom, transparent, white),
-  }
+  -webkit-mask-image: -webkit-gradient(linear, top, bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));
+  mask-image: -webkit-gradient(linear, top, bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)))
+      
+}
 .bg-opacity-gradient-to-t{
-    background: linear-gradient(to top, transparent, white),
-  }
+  -webkit-mask-image: -webkit-gradient(linear, bottom, top, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));
+  mask-image: -webkit-gradient(linear, bottom, top, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)))
+}
 </style>
