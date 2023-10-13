765aa39c0726568a06936234d968efe7d5f3a49d
Tunahan-Uysal <TunahanUysal26@gmail.com>
2023-10-13 15:52:36 +0300

    Writing out the about me gah

--- src/pages/about-me.vue
+++ src/pages/about-me.vue
@@ -1,8 +1,49 @@
 <template>
-  <div class="bg-gradient-to-b from-slate-200 to-[50vh]">
+  <div class="bg-gradient-to-b from-slate-200 to-50%">
     <UHeader />
     <div class="flex flex-row">
-      <div class="basis-8/12"></div>
+      <div class="basis-8/12 w-10/12 rounded-md h-3/4 -py-8 ml-8">
+        <section class="font-display text-2xl text-blue-500 mt-12">
+          Welcome, Traveller.
+        </section>
+        <br />
+        <div class="flex flex-row indent-8 font-article font-light">
+          <article class="basis-8/12">
+            <p>
+              I have always been awkward with introductions and it does help
+              that I do not like introducing myself either. This may be why I
+              focus more on skills, abilities and knowledge of myself and others
+              to get to know them better, but for you, and for this blog, I
+              think i'll keep it short.
+            </p>
+            <br />
+            <p>
+              I love the process of creation, I like going in steps, I like
+              going forwards with rules, and I like worlds that I can see myself
+              in. This lead me to trying to learn more about the world as we
+              know it, and it lead me to focusing more on the tangible elements
+              of Worldbuilding and the more realistic aspects that could be
+              grasped. The blogs first purpose is to express the ideas I have,
+              and the knowledge I have gained from the time I spent reading,
+              researching, creating, and assessing, but it seems to me that I
+              have trouble getting my thoughts across, so I hope you can bare
+              along with me.
+            </p>
+          </article>
+          <div class="basis-4/12">
+            <span
+              class="bg-slate-200 rounded-md w-52 mx-auto h-72 block"
+            ></span>
+          </div>
+        </div>
+        <br />
+        <article class="indent-8 font-article font-light mx-16">
+          Although what I love to do is inherently artistic, programming always
+          felt natural to me. I was always a geek, still am, but I loved the
+          process of solving the bugs, and creating a finished product. Maybe
+          this is also in line with what I like to do in worldbuilding?
+        </article>
+      </div>
       <UTredingBar />
     </div>
   </div>
