9ba3df99db610f511cc072570a9b941d3389cf8b
Tunahan-Uysal <TunahanUysal26@gmail.com>
2023-10-20 12:33:40 +0300

    Added time onto it + truncated the fat

--- src/components/UHeader.vue
+++ src/components/UHeader.vue
@@ -8,46 +8,8 @@
       class="font-display font-black text-xl text-white ml-4 basis-1/12"
       >Tunahan Uysal</NuxtLink
     >
-    <div
-      class="my-auto mr-28 bg-white h-1/2 rounded-xl shadow-xl py-0.5 mx-5 indent-3 duration-300"
-      :class="{
-        'w-96': searchBoxToggle,
-        'w-60': !searchBoxToggle,
-      }"
-      @click="console.log(isDocumentHidden)"
-    >
-      <input
-        type="search"
-        name=""
-        id=""
-        class="form-input my-0.5 w-9/12 border-r mr-2.5 outline-none duration-300"
-        maxlength="34"
-        placeholder="Search anything!"
-        ref="USearchBox"
-        :class="{
-          'w-[84%] mr-3': searchBoxToggle,
-          '': !searchBoxToggle,
-        }"
-        @focusin="searchBoxToggle = true"
-        @focusout="searchBoxToggle = isDocumentHidden"
-      />
-      <NuxtLink to="">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6 inline mb-[0.313rem] scale-90 hover:scale-100 hover:stroke-blue-950"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-          />
-        </svg>
-      </NuxtLink>
-    </div>
+
+    <USearchbox />
 
     <UBurgerButton
       @click="sideBarToggle = !sideBarToggle"
@@ -105,6 +67,7 @@
 
 <script lang="ts">
 import UBurgerButton from "~/components/UBurgerButton.vue";
+import USearchbox from "~/components/USearchbox.vue";
 import { ref, watch } from "vue";
 import Vue from "vue";
 
---
+++ src/components/USearchbox.vue
@@ -0,0 +1,86 @@
+<template>
+  <div
+    class="my-auto mr-28 bg-white h-1/2 rounded-xl shadow-xl py-0.5 mx-5 indent-3 duration-300"
+    :class="{
+      'w-96': searchBoxToggle,
+      'w-60': !searchBoxToggle,
+    }"
+    @click="console.log(isDocumentHidden)"
+  >
+    <input
+      type="search"
+      name=""
+      id=""
+      class="form-input my-0.5 w-9/12 border-r mr-2.5 outline-none duration-300"
+      maxlength="45"
+      placeholder="Search anything!"
+      ref="USearchBox"
+      :class="{
+        'w-[84%] mr-3': searchBoxToggle,
+        '': !searchBoxToggle,
+      }"
+      @focusin="searchBoxToggle = true"
+      @focusout="searchBoxToggle = isDocumentHidden"
+    />
+    <NuxtLink to="">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6 inline mb-[0.313rem] scale-90 hover:scale-100 hover:stroke-blue-950"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+        />
+      </svg>
+    </NuxtLink>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, watch } from "vue";
+import Vue from "vue";
+
+export default {
+  setup() {
+    const searchBoxToggle = ref(false);
+    const USearchBox = ref(null);
+    const isDocumentHidden = ref(false);
+
+    let isMounted = false;
+
+    onMounted(() => {
+      isMounted = true;
+    });
+
+    const handleVisChange = () => {
+      if (isMounted) {
+        isDocumentHidden.value = document.hidden;
+        searchBoxToggle.value = isDocumentHidden.value;
+        console.log(isDocumentHidden.value);
+      }
+    };
+
+    onBeforeMount(() => {
+      document.addEventListener("visibilitychange", handleVisChange);
+    });
+
+    onBeforeUnmount(() => {
+      document.removeEventListener("visibilitychange", handleVisChange);
+    });
+
+    return {
+      searchBoxToggle,
+      USearchBox,
+      isDocumentHidden,
+      handleVisChange,
+    };
+  },
+};
+</script>
+
+<style></style>
